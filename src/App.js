9137b5c798caaff45e1b6c49ecea6d3d1fb38a55
Mariano Collovatti <mariano.collovatti@gmail.com>
2023-04-04 03:11:25 -0300

    preEntrega1+Collovatti

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ItemList from "./components/ItemList/ItemList";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
+  let saludo = "BIENVENIDOS A OLA CONGELADOS";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <ItemList saludo={saludo} />
     </div>
   );
 }
---
+++ src/components/ItemList/ItemList.jsx
@@ -0,0 +1,11 @@
+const ItemList = (props) => {
+  const { saludo } = props;
+
+  return (
+    <div>
+      <h1>{saludo}</h1>
+    </div>
+  );
+};
+
+export default ItemList;
